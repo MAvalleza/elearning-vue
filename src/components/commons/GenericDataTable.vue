e01f612e63c4023c5f2290fb2427aab081553c6e
Marc Valleza <m.valleza@arcanys.com>
2023-07-05 19:24:46 +0800

    Create initial generic data table

---
+++ src/components/commons/GenericDataTable.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['update:options']);
+
+const props = defineProps({
+  headers: {
+    type: Array,
+    default: () => ([]),
+  },
+  totalItems: {
+    type: [String, Number],
+    default: 0,
+  },
+  items: {
+    type: Array,
+    default: () => ([]),
+  },
+  itemValue: {
+    type: String,
+    default: null,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const itemsPerPage = ref(10)
+
+function onUpdateOptions(event) {
+  emit('update:options', event);
+}
+</script>
+
+<template lang="pug">
+v-data-table-server(
+  v-model:items-per-page="itemsPerPage"
+  :headers="props.headers"
+  :items-length="props.totalItems"
+  :items="props.items"
+  :loading="props.loading"
+  :item-value="props.itemValue"
+  @update:options="onUpdateOptions"
+)
+</template>
