329c2c21d6284113dd523f8b6e6d6f6693bca881
Marc Valleza <m.valleza@arcanys.com>
2023-08-10 17:32:26 +0800

    Type ui store

---
+++ src/stores/ui.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia';
+import isBoolean from 'lodash-es/isBoolean';
+
+export const useUI = defineStore('ui', {
+  state: () => ({
+    loading: false,
+    snackbar: { color: '', message: '' },
+    isSnackbarVisible: false,
+  }),
+  actions: {
+    // Set loading state to boolean value
+    setLoading(val: boolean) {
+      if (!isBoolean(val)) {
+        return;
+      }
+
+      this.loading = val;
+    },
+    // Show snack bar message
+    showSnackbar({ color, message }: { color: string, message: string }) {
+      this.snackbar = { color, message };
+      this.isSnackbarVisible = true;
+    },
+  },
+});
