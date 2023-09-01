1189b4d5595b1688aa29b45dc8369310bbb65ac7
Marc Valleza <m.valleza@arcanys.com>
2023-09-01 14:10:18 +0800

    Fix authenticated check

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -18,7 +18,7 @@ export const useAuth = defineStore('auth', {
   }),
   getters: {
     isAuthenticated(state) {
-      return !isEmpty(state.currentUser);
+      return !isEmpty(state.currentUser?.id);
     },
     currentUserRole(state) {
       return state.currentUser?.role;
