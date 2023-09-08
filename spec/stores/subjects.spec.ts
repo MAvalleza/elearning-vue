c837c19a62d0e49e3bc52c882d529dea3c312067
Marc Valleza <m.valleza@arcanys.com>
2023-09-08 14:39:11 +0800

    Create subjects store unit tests

--- spec/stores/subjects.spec.ts
+++ spec/stores/subjects.spec.ts
@@ -2,11 +2,23 @@ import { setActivePinia, createPinia } from 'pinia';
 import { describe, vi, it, expect, afterEach, beforeEach } from 'vitest';
 import { useSubjects } from '../../src/stores/subjects'; // Import your store module
 import SubjectsWebservice from '../../src/webservices/subjectsWebservice';
-// import { useUI as uiStore } from '../../src/stores/ui';
 import { faker } from '@faker-js/faker';
 
-vi.mock('../../src/webservices/subjectsWebservice');
-vi.mock('../../src/stores/ui');
+vi.mock('../../src/stores/ui', () => {
+  let _cache;
+  const useUI = () => {
+    if (!_cache) {
+      _cache = {
+        showSnackbar: mockSnack,
+      }
+    }
+
+    return _cache;
+  }
+  return { useUI };
+});
+
+const mockSnack = vi.fn();
 
 describe('useSubjects', () => {
   let subjectsStore;
@@ -52,9 +64,117 @@ describe('useSubjects', () => {
     await subjectsStore.fetchSubjects(params);
 
     expect(fetchSpy).toHaveBeenCalledWith(params);
-    expect(subjectsStore.loadingSubjects).toBe(false);
     expect(subjectsStore.subjects).toEqual(mappedSubjects);
     expect(subjectsStore.subjectsTotal).toBe(1);
     expect(subjectsStore.subjectsCurrentPage).toBe(1);
+  }); 
+
+  it('should show a notification when there is an error', async () => {
+    const mockResponse = {
+      errors: ['Some error'],
+    };
+    
+    const fetchSpy = vi.spyOn(SubjectsWebservice.prototype, 'getSubjects');
+    fetchSpy.mockResolvedValue(mockResponse);
+  
+    await subjectsStore.fetchSubjects({});
+
+    expect(mockSnack).toHaveBeenCalledWith({
+      color: 'error',
+      message: mockResponse.errors[0],
+    })
+  })
+
+  it('should create subject', async () => {
+    const mockResponse = {
+      id: 'some_id'
+    };
+    const params = { some: 'param' };
+
+    const createSpy = vi.spyOn(SubjectsWebservice.prototype, 'createSubject');
+    createSpy.mockResolvedValue(mockResponse);
+
+    await subjectsStore.createSubject(params);
+
+    expect(createSpy).toHaveBeenCalledWith(params);
+
+    expect(mockSnack).toHaveBeenCalledWith({
+      color: 'success',
+      message: 'Successfully created a subject.',
+    });
   });
+
+  it('should fetch specific subject', async () => {
+    const subjectId = 'some_id';
+    const mockResponse = {
+      id: subjectId
+    };
+    const params = { some: 'param' };
+
+    const getSpy = vi.spyOn(SubjectsWebservice.prototype, 'getSubject');
+    getSpy.mockResolvedValue(mockResponse);
+
+    await subjectsStore.fetchSubject(subjectId, params);
+
+    expect(getSpy).toHaveBeenCalledWith({
+      id: subjectId,
+      params
+    });
+
+    expect(subjectsStore.currentSubject).toEqual(mockResponse);
+  });
+
+  it('should update subject', async () => {
+    const subjectId = 'some_id';
+    const mockResponse = {
+      id: subjectId
+    };
+    const params = { some: 'param' };
+
+    const updateSpy = vi.spyOn(SubjectsWebservice.prototype, 'updateSubject');
+    updateSpy.mockResolvedValue(mockResponse);
+
+    await subjectsStore.updateSubject(subjectId, params);
+
+    expect(updateSpy).toHaveBeenCalledWith(
+      subjectId,
+      params
+    );
+
+    expect(mockSnack).toHaveBeenCalledWith({
+      color: 'success',
+      message: 'Successfully updated the subject.',
+    });
+  });
+
+  it('should delete subject', async () => {
+    const subjectId = 'some_id';
+    const mockResponse = {
+      id: subjectId
+    };
+
+    const deleteSpy = vi.spyOn(SubjectsWebservice.prototype, 'deleteSubject');
+    deleteSpy.mockResolvedValue(mockResponse);
+
+    await subjectsStore.deleteSubject(subjectId);
+
+    expect(deleteSpy).toHaveBeenCalledWith(subjectId);
+
+    expect(mockSnack).toHaveBeenCalledWith({
+      color: 'success',
+      message: 'Successfully deleted the subject.',
+    });
+  });
+
+  it('should indicate deletion when delete table action is selected', async () => {
+    const subjectId = 'some_id';
+    const action = 'delete';
+
+    const result = await subjectsStore.onTableAction({ id: subjectId, action });
+
+    expect(result).toEqual({
+      id: subjectId,
+      delete: true
+    }); 
+  })
 });
