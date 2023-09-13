40bea81c2d7c8e96748efc505e92c38d18660808
Marc Valleza <m.valleza@arcanys.com>
2023-09-13 18:45:09 +0800

    Fix import of blot formatter

--- src/components/commons/TextEditor.vue
+++ src/components/commons/TextEditor.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, type Ref, computed } from 'vue';
 import { Delta, QuillEditor } from '@vueup/vue-quill';
-import BlotFormatter from 'quill-blot-formatter';
+import BlotFormatter from 'quill-blot-formatter/dist/BlotFormatter';
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 const props = defineProps({
